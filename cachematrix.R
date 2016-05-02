9606d30b3ca579eed949b390bf5b5d2d18da3e93
TSBPhD <TSBPhD@users.noreply.github.com>
2016-05-01 20:21:14 -0400

    Replaced content of cachematrix.R with the assignment source code for submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+# 02.Wk3 - Assignment 2
+# Caching the Inverse of a Matrix
+# Your assignment is to write a pair of functions that cache the inverse of a
+# matrix.
 
-## Write a short comment describing this function
+# The following solutions are based on the examples given in course assigment.
+## https://www.coursera.org/learn/r-programming/peer/tNy8H/programming-assignment-2-lexical-scoping
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## 1. makeCacheMatrix: This function creates a special "matrix" object that can 
+## cache its inverse.
+makeCacheMatrix <- function(x = martix()){
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# 2. cacheSolve: This function computes the inverse of the special "matrix"
+# returned by makeCacheMatrix above. If the inverse has already been calculated
+# (and the matrix has not changed), then the cachesolve should retrieve the
+# inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("Getting cached data...")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+# To test, source the above code and then run the following to create a test
+# matrix:
+testMatrix <- matrix(c(1:4),2,2)
+
+# Next, create an object to hold the special "matrix" object from (1) above:
+mCm <- makeCacheMatrix(testMatrix)
+
+# Finally, call cacheSolve with that object:
+cacheSolve(mCm)
+
+# To see the notification that data is coming from cache, repeat the above
+# command:
+cacheSolve(mCm)
